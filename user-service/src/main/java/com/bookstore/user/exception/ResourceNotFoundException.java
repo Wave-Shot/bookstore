8a7a3d216c0f9256baaacb6888e23005ba400a7b
Wave-Shot <vsh.2509@gmail.com>
2026-04-29 11:34:25 +0530

    [Srihari]: Completed working admin-service

--- user-service/src/main/java/com/bookstore/user/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.user.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
