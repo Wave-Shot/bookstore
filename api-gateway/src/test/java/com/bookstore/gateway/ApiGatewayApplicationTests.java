50b9e637534c023f1276ceaed56c4b78e465bfc2
Wave-Shot <vsh.2509@gmail.com>
2026-04-29 11:35:27 +0530

    [Srihari]: Completed API Gateway

--- api-gateway/src/test/java/com/bookstore/gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/bookstore/gateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bookstore.admin;
+package com.bookstore.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AdminServiceApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
